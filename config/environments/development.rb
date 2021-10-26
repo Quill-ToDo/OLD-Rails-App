805c1175edab6c7db9ed4566a187bfae21d1749a
Raluca Ghilea <ralucaghilea@ralucas-mbp.colgate.edu>
2021-10-26 18:15:10 -0400

    #5 create user model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+require "action_mailer/railtie"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -68,4 +69,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
