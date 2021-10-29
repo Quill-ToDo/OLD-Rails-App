fc1d4e906c71b83aa9df1a542f819de9e45cd297
jamesdaus <jamesdaus8@gmail.com>
2021-10-29 05:16:25 -0400

    Add all other routes boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'tasks/index'
+  get 'tasks/new'
+  get 'tasks/create'
+  get 'tasks/show'
+  get 'tasks/edit'
+  get 'tasks/update'
+  get 'tasks/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :tasks
   root 'tasks#index'
