470c87a5e4b0f6be57fc07e7ce13e29170b72e77
jamesdaus <jamesdaus8@gmail.com>
2021-10-29 05:04:47 -0400

    Generated controller and crudi routes

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,16 @@
+class TasksController < ApplicationController
+  def index
+  end
+  def new
+  end
+  def create
+  end
+  def show
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+end
