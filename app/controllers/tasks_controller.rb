89a555819ad0d100b7bdbdc8e8ebe839ed448697
Colin <ckjones@colgate.edu>
2021-10-30 16:14:52 -0400

    task controller first steps

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,18 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(project_params)
+    if @task.save
+      flash[:notice] = "New task #{@task.title} created"
+      redirect_to
+    # validations
   end
 
   def show
@@ -19,4 +26,9 @@ class TasksController < ApplicationController
 
   def destroy
   end
+
+  private
+    def set_task
+      @task = Task.find(params[:id])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_220143) do
+ActiveRecord::Schema.define(version: 2021_10_28_014000) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "start"
+    t.datetime "due", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,15 +31,6 @@ ActiveRecord::Schema.define(version: 2021_10_26_220143) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "start"
-    t.datetime "due", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
