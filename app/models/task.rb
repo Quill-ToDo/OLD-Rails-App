cb15ac6e9871dfc39d8b619ce1816db77549891d
Devon Lee <dslee@colgate.edu>
2021-11-06 17:25:26 -0400

    update gemfile lock

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :description, length: { maximum: 1000 }
+  validates :due, presence: true
+  validate :due_after_start?
+
+  private
+  def due_after_start?
+    if !due.nil?
+      errors.add(:due, 'due date must be after start date') if !start.nil? && !due.after?(start)
+    end
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
+RSpec.describe 'Task', type: :model do
   describe 'check attributes and methods' do
     it 'should be able to create a Task object which has the correct methods on it' do
       dt = DateTime.new
-      task = Task.create!(title: 'test', description: 'desc', start: dt.noon, due: dt.midnight)
+      task = Task.create!(title: 'test', description: 'desc', start: dt.midnight, due: dt.noon)
       expect(task).to respond_to :title
       expect(task).to respond_to :description
       expect(task).to respond_to :start
@@ -14,6 +14,10 @@ RSpec.describe Task, type: :model do
 
   it "should fail to create a Task if there is no due date" do
     dt = DateTime.new
-    expect{Task.create!(title: 'test', description: 'desc', start: dt.noon)}.to raise_exception ActiveRecord::NotNullViolation
+    expect{Task.create!(title: 'test', description: 'desc', start: dt.noon)}.to raise_exception ActiveRecord::RecordInvalid
+  end
+  it "should fail to create a Task if maximum text inputs exceed" do
+    dt = DateTime.new
+    expect{Task.create!(title: '1'*1000, description: '1'*10000, due: dt.noon)}.to raise_exception ActiveRecord::RecordInvalid
   end
 end
