1f34335d89529478a9e2685a1642c6b472b57b56
Raluca Ghilea <ralucaghilea@ralucas-mbp.colgate.edu>
2021-11-06 17:58:56 -0400

    Eliminated redundant eventListener

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,19 +29,3 @@ document.addEventListener('DOMContentLoaded', function() {
     // });
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      events: "/tasks/get_tasks"
-      // events: [
-      //   { // this object will be "parsed" into an Event Object
-      //     title: 'First Event Ever!!', // a property!
-      //     start: '2021-11-05', // a property!
-      //     end: '2021-11-07' // a property! ** see important note below about 'end' **
-      //   }
-      // ]
-    });
-    calendar.render();
-  });
-
