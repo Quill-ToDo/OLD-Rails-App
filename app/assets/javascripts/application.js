422048b9dc670eb4c0315c3fa54ae6bc3a3a0081
Raluca Ghilea <ralucaghilea@ralucas-mbp.colgate.edu>
2021-11-12 17:04:42 -0500

    date click function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,43 @@ var calendar;
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     calendar = new FullCalendar.Calendar(calendarEl, {
+      selectable: true,
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      select: function(info) {
+        console.log(info);
+        var title = prompt('Enter Title');
+        if(title){
+          // AJAX call to DB
+          jQuery.get(
+            "tasks/create", 
+            {
+              task: {
+                title: title,
+                start: info.start,
+                due: info.end
+              }
+            }
+          );
+          //render
+          calendar.render();
+          // calendar.addEvent({
+          //   title: title,
+          //   start: info.start,
+          //   allDay: true
+          // });
+        }
+        console.log(title);
+        var newEvent = new Object();
+        newEvent.title = title;
+        newEvent.start = moment(info.start).format();
+        console.log(newEvent)
+        console.log(this)
+        // alert('selected ' + info.startStr + ' to ' + info.endStr);
+      },
       initialView: 'dayGridMonth',
       events: "/tasks/get_tasks"
       // events: [
