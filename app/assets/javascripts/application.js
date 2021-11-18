583227fcbff8d47d9df5847363c1c454cf98106b
Lily Davisson <lilydavisson@gmail.com>
2021-11-18 17:01:42 -0500

    Make callback work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,52 +12,49 @@ var calendar;
 
 document.addEventListener('DOMContentLoaded', function () {
   var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      selectable: true,
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-      },
-      eventSources: [
-        {
-          url: '/tasks/calendar_tasks',
-          method: 'GET',
-          failure: function() {
-            alert('there was an error while fetching tasks!');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    selectable: true,
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    eventSources: [{
+      url: '/tasks/calendar_tasks',
+      method: 'GET',
+      failure: function () {
+        alert('there was an error while fetching tasks!');
+      }
+    }],
+    select: function (info) {
+      var title = prompt('Enter Title');
+      var task = {
+        title: title,
+        start: info.start,
+        due: info.end,
+        calendar: true
+      };
+      if (title) {
+        jQuery.post(
+          "/tasks", {
+            task: task
           }
-        }
-      ],
-      select: function(info) {
-        var title = prompt('Enter Title');
-        if(title){
-          jQuery.post(
-            "/tasks", 
-            {
-              task: {
-                title: title,
-                start: info.start,
-                due: info.end,
-                calendar: true
-              }
-            }
-          );
-          calendar.refetchEvents();
-          calendar.unselect();
-        }
-      },
-      initialView: 'dayGridMonth',
-      expandRows: true,
-      height: "100%",
-      dayCellClassNames: 'dark-section',
-      eventAdd: reRenderList,
-      eventRemove: reRenderList,
-      eventChange: reRenderList
-    });
-    calendar.render();
+        );
+        calendar.addEvent(task)
+        calendar.unselect();
+      }
+    },
+    initialView: 'dayGridMonth',
+    expandRows: true,
+    height: "100%",
+    dayCellClassNames: 'dark-section',
+    eventAdd: reRenderList,
+    eventRemove: reRenderList,
+    eventChange: reRenderList
+  });
+  calendar.render();
 });
 
-
-function reRenderList(events) {
-  $.get("/tasks/tasks_update_partials");
+function reRenderList() {
+  $.get("/tasks/update_partials");
 }
