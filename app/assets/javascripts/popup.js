c02e83f3221151bf10a5d8730ad241242d04f562
Devon Lee <dslee@colgate.edu>
2021-11-21 20:16:00 -0500

    stylesheets and js

---
+++ app/assets/javascripts/popup.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function(){
+    var modal = $("#modalDialog");
+    var btn = $("#btn-add");
+    var span = $("#close");
+    btn.on('click', function(){
+        modal.toggle();
+    });
+    span.on('click', function(){
+        modal.hide();
+    });
+});
