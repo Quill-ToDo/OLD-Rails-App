18fa3bd40877f2eabbbaefecd61d438ad40f2e2c
Lily Davisson <lilydavisson@gmail.com>
2021-11-23 19:26:04 -0500

    Make alerts appear bottom right

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,60 +29,71 @@ document.addEventListener('DOMContentLoaded', function () {
       var title = prompt('Enter Title');
       if (title) {
         jQuery.post(
-          "/tasks", {
-            task: {
-              title: title,
-              start: info.start,
-              due: info.end,
-              calendar: true
-            }
-          }).done(function() {
+            "/tasks", {
+              task: {
+                title: title,
+                start: info.start,
+                due: info.end,
+                calendar: true
+              }
+            }).done(function () {
             calendar.refetchEvents();
             reRenderList();
           })
-          .fail(function() {
-            alert( "ERROR: Task could not be created!" );
+          .fail(function () {
+            alert("ERROR: Task could not be created!");
           })
-          .always(function() {
+          .always(function () {
             calendar.unselect();
           });
-        }
-      },
-      editable: true,
-      droppable: true,
-      eventDrop: function(info) {
-        if(info.event.id != undefined){
-          $.ajax({
-            url: '/tasks/' + info.event.id,
-            type: 'PATCH',
-            data: {
-              task: {
-                title: info.event.title,
-                description: info.event._def.extendedProps.description,
-                start: info.event.start,
-                due: info.event.end,
-                calendar: true,
-                update: true
-              }
-            },
-            success: function() {
-              calendar.refetchEvents();
-              reRenderList();
-            },
-            error: function() {
-              alert( "ERROR: Task information could not be updated!" );
+      }
+    },
+    editable: true,
+    droppable: true,
+    eventDrop: function (info) {
+      if (info.event.id != undefined) {
+        $.ajax({
+          url: '/tasks/' + info.event.id,
+          type: 'PATCH',
+          data: {
+            task: {
+              title: info.event.title,
+              description: info.event._def.extendedProps.description,
+              start: info.event.start,
+              due: info.event.end,
+              calendar: true,
+              update: true
             }
-          });
-        }
-      },
-      initialView: 'dayGridMonth',
-      expandRows: true,
-      height: "100%",
-      dayCellClassNames: 'dark-section'
-    });
-    calendar.render();
+          },
+          success: function () {
+            calendar.refetchEvents();
+            reRenderList();
+          },
+          error: function () {
+            alert("ERROR: Task information could not be updated!");
+          }
+        });
+      }
+    },
+    initialView: 'dayGridMonth',
+    expandRows: true,
+    height: "100%",
+    dayCellClassNames: 'dark-section'
+  });
+  calendar.render();
+
+  handleAlerts();
 });
 
 function reRenderList() {
   $.get("/tasks/update_partials")
+}
+
+// Alerts 
+
+function handleAlerts() {
+  // delete alert on exit click
+  $(".alert-pop-up img").on("click", function () {
+    $(this).parent().remove()
+  })
 }
