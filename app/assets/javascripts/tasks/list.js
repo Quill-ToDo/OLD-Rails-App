a057c4f685de047302869327f322d79c3dee36e5
Lily Davisson <lilydavisson@gmail.com>
2021-12-02 00:27:38 -0500

    list

--- app/assets/javascripts/tasks/list.js
+++ app/assets/javascripts/tasks/list.js
@@ -1,20 +1,55 @@
 
 document.addEventListener('DOMContentLoaded', function () {
+    completeTaskHandler();
+    // showPopupHandler();
+});
 
+
+function showPopupHandler() {
+    // Render popup for this task on click
+    $(".task-wrapper .title a").on("click", function () {
+        console.log("click on task name");
+        var id = $(this).attr("data-task-id");
+        $.get({
+            url: `/tasks/${id}`,
+            dataType: "json",
+            success: function (data) {
+                $("#show-wrapper").html(data.html);
+                $("#show-wrapper").toggle();
+                completeTaskHandler();
+            }
+        });
+
+        // Hide and remove this handler on clicking something other than the show popup
+        $("#show-wrapper").on('click', function (e) {
+            if ($(e.target).closest("#show-wrapper .mid-section").length === 0) {
+                console.log("click off show");
+                $("#show-wrapper").toggle();
+                // $("#show-wrapper").off('click');
+            }
+        });
+    });
+}
+
+function completeTaskHandler(successCallback) {
     // Update task as completed on click
     $(".check-box-wrapper:first-child input").on("click", function () {
         var taskId = $(this).attr("data-task-id");
+        console.log("completeTask")
         // Add or remove strike-through
+
+        // Update task as complete
+        $.post("/tasks/complete_task", {
+            id: taskId,
+            success: successCallback
+        });
+
         if ($(this).prop("checked")) {
             $(`.title a[data-task-id='${taskId}']`).addClass("completed");
         } else {
             $(`.title a[data-task-id='${taskId}']`).removeClass("completed");
         }
 
-        // Update task as complete
-        $.post("/tasks/complete_task", {
-            id: taskId
-        })
     });
 
     // Collapse sections on click 
@@ -29,4 +64,15 @@ document.addEventListener('DOMContentLoaded', function () {
             symbol.css("rotate", "45deg");
         }
     });
-});
+}
+
+function reRenderList() {
+    console.log("rerender list");
+    $.get({
+        url: "/tasks/update_partials",
+        dataType: "json",
+        success: function (data) {
+            $("#list-wrapper").html(data.html);
+        }
+    });
+}
