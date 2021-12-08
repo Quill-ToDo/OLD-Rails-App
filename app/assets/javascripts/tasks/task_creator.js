62e3a4957600868803ddadf767ada3e6ee829cb1
Lily Davisson <lilydavisson@gmail.com>
2021-12-08 00:10:34 -0500

    Style datepicker

--- app/assets/javascripts/tasks/task_creator.js
+++ app/assets/javascripts/tasks/task_creator.js
@@ -1,52 +1,24 @@
-//= require jquery3
 //= require popper
-//= require bootstrap-sprockets
-//= require moment
-//= require tempusdominus-bootstrap-4.js
-//= require jquery_ujs
+//= require tempus-dominus
 
-$('#datetime-picker-start').datetimepicker({
-  format: moment().format(),
-  icons: {
-    time: 'far fa-clock',
-    date: 'far fa-calendar',
-    up: 'fas fa-arrow-up',
-    down: 'fas fa-arrow-down',
-    previous: 'fas fa-chevron-left',
-    next: 'fas fa-chevron-right',
-    today: 'far fa-calendar-check',
-    clear: 'fas fa-trash',
-    close: "fas fa-times"
-  },
-  buttons: {
-    showClose: true
+document.addEventListener('DOMContentLoaded', function () {
+  // Only run if body has tasks class
+  if ($('body.tasks').length) {
+    new tempusDominus.TempusDominus(document.getElementById('datetime-picker-start'));
+    new tempusDominus.TempusDominus(document.getElementById('datetime-picker-due'));
   }
+
+  $(document).on("click", function (e) {
+    if (e.target) {
+      let target = e.target;
+      calendarButtonHandler(target, e);
+    }
+  });
 });
 
-$('#datetime-picker-due').datetimepicker({
-  // format: 'YYYY-MM-DDTHH:mm:ssZ',
-  format: moment().format(),
-  icons: {
-    time: 'far fa-clock',
-    date: 'far fa-calendar',
-    up: 'fas fa-arrow-up',
-    down: 'fas fa-arrow-down',
-    previous: 'fas fa-chevron-left',
-    next: 'fas fa-chevron-right',
-    today: 'far fa-calendar-check',
-    clear: 'fas fa-trash',
-    close: "fas fa-times"
-  },
-  buttons: {
-    showClose: true
+function calendarButtonHandler(target, e) {
+  var input_box = $(target).parents(".input-group-text");
+  if (input_box.length) {
+    e.preventDefault();
   }
-});
-
-$("#datetime-picker-start").on("change.datetimepicker", function (e) {
-  $('#datetime-picker-due').datetimepicker('minDate', e.date);
-  console.log(e.date);
-});
-
-$("#datetime-picker-due").on("change.datetimepicker", function (e) {
-  $('#datetime-picker-start').datetimepicker('maxDate', e.date);
-});
+}
