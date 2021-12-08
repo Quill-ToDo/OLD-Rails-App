30eb66444b90d89e3da4268bde5bff5901a9ef1e
Lily Davisson <lilydavisson@gmail.com>
2021-12-08 00:32:38 -0500

    set options

--- app/assets/javascripts/tasks/task_creator.js
+++ app/assets/javascripts/tasks/task_creator.js
@@ -4,21 +4,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Only run if body has tasks class
   if ($('body.tasks').length) {
-    new tempusDominus.TempusDominus(document.getElementById('datetime-picker-start'));
-    new tempusDominus.TempusDominus(document.getElementById('datetime-picker-due'));
-  }
-
-  $(document).on("click", function (e) {
-    if (e.target) {
-      let target = e.target;
-      calendarButtonHandler(target, e);
+    var options = {
+      display: {
+        buttons: {
+          close: true
+        },
+        // toolbarPlacement: "top"
+      }
     }
-  });
-});
-
-function calendarButtonHandler(target, e) {
-  var input_box = $(target).parents(".input-group-text");
-  if (input_box.length) {
-    e.preventDefault();
+    new tempusDominus.TempusDominus(document.getElementById('datetime-picker-start'), options);
+    new tempusDominus.TempusDominus(document.getElementById('datetime-picker-due'), options);
   }
-}
+});
